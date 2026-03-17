b1ddf7d203750bd23ef65938449fb3b45f8a8a79
e330168 <e330168@gmail.com>
2026-03-17 14:47:27 +0100

    Add Sessions Data and Sessions Page

---
+++ src/pages/Sessions.tsx
@@ -0,0 +1,17 @@
+import {SESSIONS} from "../dummy-sessions";
+
+export default function Sessions(){
+    return(
+        <main id="sessions-page">
+            <header>
+                <h2>Available mentoring sessions</h2>
+                     <p>
+                        From an one-on-one introduction to React's basics all the way up to a
+                        deep dive into state mechanics - we got just the right session for
+                        you!
+                     </p>
+            </header>
+                {/* <SessionsList sessions={SESSIONS}/> */}
+        </main>
+    )
+}
