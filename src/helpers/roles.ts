740913c32dfe2fa78cce0e519d781c07ddaf1e6b
e330168 <e330168@gmail.com>
2026-03-19 15:05:39 +0100

    Centralised Roles & Permission Setup

---
+++ src/helpers/roles.ts
@@ -0,0 +1,34 @@
+export const ROLES={
+    ADMIN: "admin",
+    MANAGER: "manager",
+    USER: "user",
+    GUEST: "guest"
+}
+
+export const PERMISSIONS={
+    VIEW_DASHBOARD: "view_dashboard",
+    VIEW_SESSIONS: "view_sessions",
+    EDIT_SESSION: "edit_session",
+    DELETE_SESSION: "delete_session"
+}
+
+export const ROLES_PERMISSIONS: Record<string,string[]> = {
+    [ROLES.ADMIN]: [
+        PERMISSIONS.VIEW_DASHBOARD,
+        PERMISSIONS.VIEW_SESSIONS,
+        PERMISSIONS.EDIT_SESSION,
+        PERMISSIONS.DELETE_SESSION
+    ],
+    [ROLES.MANAGER]: [
+        PERMISSIONS.VIEW_DASHBOARD,
+        PERMISSIONS.VIEW_SESSIONS,
+        PERMISSIONS.EDIT_SESSION
+    ],
+    [ROLES.USER]: [
+        PERMISSIONS.VIEW_DASHBOARD,
+        PERMISSIONS.VIEW_SESSIONS
+    ],
+    [ROLES.GUEST]: [
+        PERMISSIONS.VIEW_DASHBOARD
+    ]
+}
