a4b6fa0b098cf53ae905745be96ee65060dccb33
e330168 <e330168@gmail.com>
2026-03-22 16:13:26 +0100

    Session Route Implementation

--- src/pages/Sessions.tsx
+++ src/pages/Sessions.tsx
@@ -1,10 +1,18 @@
 import SessionsList from "../components/Sessions/SessionsList";
+import { useAuth } from "../context/useAuth";
 import {SESSIONS} from "../dummy-sessions";
 
 export default function Sessions(){
+
+    const{user}= useAuth();
+    console.log(user);
+
+
     return(
         <main id="sessions-page">
             <header>
+                <h1> Welcome back, {user?.username.toUpperCase()} !</h1>
+                <h2>Role: {user?.role}</h2>
                 <h2>Available mentoring sessions</h2>
                      <p>
                         From an one-on-one introduction to React's basics all the way up to a
